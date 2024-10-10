9d2879ecf3060f09561134f6e5504fad34ce3eda
Cheaucapik <oceanehuynh711@mail.com>
2024-10-10 23:08:26 +0200

    V1.17 Commentaires

--- S101.c
+++ S101.c
@@ -9,19 +9,19 @@ typedef struct{
     char nom_etu[NOM_MAX];
     unsigned int num_grp;
     unsigned int id_etu_tab;
-}Etudiant;
+}Etudiant; //données concernant l'étudiant
 
 typedef struct{
     unsigned int num_jour;
     char demi_journee[3];
     unsigned int id_abs_tab;
-}Absence;
+}Absence; //données concernant les absences d'un étudiant
 
 typedef struct{
     unsigned int id_etu, id_abs; //permet l'incrémentation sans la modification des valeurs dans la fonction
     Absence tab_absence[MAX_ABS]; //tableau pour contenir toutes les données de la commande absence
     Etudiant tab_etudiant[MAX_ETU]; //tableau pour contenir toutes les données de la commande etudiant
-}Donnees;
+}Donnees; //struct évitant les variables globales
 
 //fonctions prototypes
 int execution(char *commande, Donnees *donnees); //exécute une commande par comparaison
@@ -40,7 +40,7 @@ int main(int argc, char *argv[]){
                 strcpy(commande, argv[1]); //si l'entrée n'est pas nulle
             } else {
                 fgets(commande, sizeof(commande), stdin);
-                commande[strcspn(commande, "\n")] = 0; //évite de prendre en compte la remise à la ligne
+                commande[strcspn(commande, "\n")] = 0; //évite de prendre en compte la remise à la ligne du fgets
                 execution(commande, &donnees); //execution de la commande
             }
     }
@@ -52,13 +52,13 @@ int execution(char *commande, Donnees *donnees){ //exécute une commande par com
         unsigned int num_grp;
         unsigned int num_jour = 1;
         char demi_journee[3];
-        unsigned temp_id_etu;
+        unsigned temp_id_etu; //utile dans la commande absence, désigne l'entrée de l'utilisateur
         unsigned int num_jour_courant = 1;
 
     if(strcmp(commande, "exit") == 0){ //C0 : exit
         exit(0); //arrêt du programme
     }
-    else if(sscanf(commande, "inscription %30s %u", nom_etu, &num_grp) == 2){ //C1 : inscription
+    else if(sscanf(commande, "inscription %29s %u", nom_etu, &num_grp) == 2){ //C1 : inscription
         inscription(nom_etu, num_grp, donnees);
     }
     else if (sscanf(commande, "absence %u %u %2s", &temp_id_etu, &num_jour, demi_journee) == 3) { //C2 : absence
@@ -85,10 +85,10 @@ int inscription(char nom_etu[NOM_MAX], unsigned int num_grp, Donnees *donnees){ 
         }
     }
     printf("Inscription enregistree (%u)\n", donnees->id_etu);
-    strcpy(donnees->tab_etudiant[donnees->id_etu].nom_etu, nom_etu); //copie le nom de l'étudiant dans un tableau (meme position que tout le reste) 
-    donnees->tab_etudiant[donnees->id_etu].num_grp = num_grp; //copie le groupe de l'étudiant dans un tableau (meme position que tout le reste)
-    donnees->tab_etudiant[donnees->id_etu].id_etu_tab = donnees->id_etu; //copie l'id de l'etudiant dans un tableau pour les autres commandes
-    ++donnees->id_etu; //incrémente l'id de l'étudiant à chaque fois
+    strcpy(donnees->tab_etudiant[donnees->id_etu].nom_etu, nom_etu); //le nom de l'tudiant est copié dans un tableau se trouvant à la même place que l'id de l'étudiant pour pouvoir attribuer ce nom à un étudiant(id)
+    donnees->tab_etudiant[donnees->id_etu].num_grp = num_grp; //le numéro de groupe est copié dans un tableau se trouvant à la même place que l'id de l'étudiant pour pouvoir attribuer ce numéro à un étudaint(id)
+    donnees->tab_etudiant[donnees->id_etu].id_etu_tab = donnees->id_etu; //l'id de l'étudiant est copié dans un tableau à la même place que son id. Ainsi on a l'id 1 qui se trouve à la position 1 du tableau, permettant une gestion plus facile des données
+    ++donnees->id_etu; //incrémente l'id de l'étudiant à chaque nouvelle inscription
     return 0;
 }
 
@@ -119,29 +119,30 @@ int absence(int temp_id_etu, int num_jour, char demi_journee[3], Donnees *donnee
         }
     }
     printf("Absence enregistree [%u]\n", donnees->id_abs);
-    donnees->tab_absence[donnees->id_abs].id_abs_tab = temp_id_etu; //copie l'id d'absence de l'étudiant dans un tableau (meme position que tout le reste)
-    donnees->tab_absence[donnees->id_abs].num_jour = num_jour; //copie le num_jour de l'absence de l'étudiant dans un tableau (meme position que tout le reste)
-    strcpy(donnees->tab_absence[donnees->id_abs].demi_journee, demi_journee); //copie la demi_journee d'absence de l'étudiant dans un tableau (meme position que tout le reste)
-    ++donnees->id_abs; //incrémente l'id de l'absence
+    donnees->tab_absence[donnees->id_abs].id_abs_tab = temp_id_etu; //l'id de l'absence est copiée dans un tableau où elle dépend de l'id de l'étudiant pour pouvoir attribuer une absence à un étudiant, tout cela à la position id_abs
+    donnees->tab_absence[donnees->id_abs].num_jour = num_jour; //le numéro de jour est copié dans un tableau se trouvant à la même place que l'absence pour pouvoir attribuer ce numéro à l'absence
+    strcpy(donnees->tab_absence[donnees->id_abs].demi_journee, demi_journee); //la demi-journée est copiée dans un tableau se trouvant à la même place que l'absence pour pouvoir attribuer cette demi-journée à l'absence
+    ++donnees->id_abs; //incrémente l'id de l'absence à chaque nouvelle absence
 
     return 0;
 }
 
 int etudiants(int num_jour_courant, Donnees *donnees){
-    qsort(donnees->tab_etudiant + 1, donnees->id_etu - 1, sizeof(Etudiant), compare); //trie le tableau d'étudiants
+    qsort(donnees->tab_etudiant + 1, donnees->id_etu - 1, sizeof(Etudiant), compare); //trie le tableau d'étudiants dans un ordre croissant (par groupe), puis par ordre alphabétique
     if(num_jour_courant < 1){
         printf("Date incorrecte\n");
         return 0;
     }
+
     for(int i = 1; i < donnees->id_etu; ++i){
-        unsigned int total_abs = 0;
+        unsigned int total_abs = 0; //le total d'absences revient à 0 pour chaque nouveau étudiant
         for(int j = 1; j < donnees->id_abs; ++j){
             if(donnees->tab_absence[j].id_abs_tab == donnees->tab_etudiant[i].id_etu_tab &&
                donnees->tab_absence[j].num_jour <= num_jour_courant){
-                total_abs++;
+                total_abs++; //incrémente le total d'absence si on trouve une absence (pour l'étudiant) pour le num de jour courant supérieur ou égal au num de jour de l'absence
             }
         }
-        printf("(%u) %-30s %10u %5u\n", donnees->tab_etudiant[i].id_etu_tab, donnees->tab_etudiant[i].nom_etu, donnees->tab_etudiant[i].num_grp, total_abs);
+        printf("(%u) %-30s %10u %5u\n", donnees->tab_etudiant[i].id_etu_tab, donnees->tab_etudiant[i].nom_etu, donnees->tab_etudiant[i].num_grp, total_abs); //printf pour chaque étudiant, ligne par ligne
     }
     return 0;
 }
@@ -149,12 +150,14 @@ int etudiants(int num_jour_courant, Donnees *donnees){
 int compare(const void *a, const void *b){
     Etudiant *etudiantA = (Etudiant *)a;
     Etudiant *etudiantB = (Etudiant *)b;
-    if (etudiantA->num_grp > etudiantB->num_grp) { //si le num_grp de l'étudiant A < num_grp de l'étudiant
+    if (etudiantA->num_grp < etudiantB->num_grp) { //si le num_grp de l'étudiant A < num_grp de l'étudiant
+        return -1;
+    } //num_grp de l'étudiant A est bien inférieur à celui de l'étudiant B
+    else if(etudiantA->num_grp > etudiantB->num_grp) { //si le num_grp de l'étudiant A < num_grp de l'étudiant
         return 1; //num_grp de l'étudiant A est bien inférieur à celui de l'étudiant B
-    } else if (etudiantA->num_grp < etudiantB->num_grp) { //si le num_grp de l'étudiant A < num_grp de l'étudiant
-        return -1; //num_grp de l'étudiant A est bien inférieur à celui de l'étudiant B
-    } else{
-        return strcmp(etudiantA->nom_etu, etudiantB->nom_etu); //Ordre alphabétique si même grp
+    } 
+    else{
+        return strcmp(etudiantA->nom_etu, etudiantB->nom_etu); //Ordre alphabétique (les num_grp de l'étudiant A et B sont identiques)
     }
 }
 
